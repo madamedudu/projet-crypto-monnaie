8d0a66463d855c77af00526a832005e4b6717721
ZyadMA <zyad.mohamed-abdelmoneim@univ-tlse3.fr>
2026-03-22 21:15:00 +0100

    jai change define

--- include/blockchain.h
+++ include/blockchain.h
@@ -1,8 +1,8 @@
-#ifndef BLOCKCHAIN_H
-#define BLOCKCHAIN_H
+// #ifndef BLOCKCHAIN_H
+// #define BLOCKCHAIN_H
 
-#include "define.h"
+// #include "define.h"
 
-void merkle_root(Transaction transactions[], int nb_transactions, char root[65]);
+// void merkle_root(Transaction transactions[], int nb_transactions, char root[65]);
 
-#endif
+// #endif
--- include/define.h
+++ include/define.h
@@ -1,33 +1,106 @@
-/*
-le define.h va permettre de créer les structures de données utilisées pas tous les fichiers .c du projet,
-
-*/
 #ifndef DEFINE_H
 #define DEFINE_H
-
+// Header donné à titre indicatif (en clair vous pouvez l'ignorer et faire le vôtre)
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <string.h>
 #include <time.h>
+#include "sha256.h"
+#include "sha256_utils.h"
 
-// constantes
-#define MAX_TRANSACTIONS 10
-#define HASH_SIZE 32 // taille du hash en bytes 32 bytes = 256 bits
-#define ADRESS_SIZE 50  
+#define MAX_BUF 1024
+#define MAX_STRING 64
+#define MAX_BLOCK 15
+#define MAXTX 10 // nb tx par bloc (tests)
+#define DIFFICULTY 4 // difficulté pour le minage
+#define INITIALREWARD 50*1000 // montant de départ de la récompence des mineurs
+#define HELIREWARD 100*1000 // montant de l'helicopter money
+#define MAX_USERS 3 // nb utilisateurs (tests)
+#define LOCK_SCRIPT_SIZE 4 // (parties 2 et 3)
+#define UNLOCK_SCRIPT_SIZE 3 // (parties 2 et 3)
+#define FEE_RATE 5 //%
+#define HALVING 10 // nombre de blocs pour diviser la récompence
+#define HASHLENGTH SHA256_BLOCK_SIZE*2 + 1
 
-//structure de données pour le hash
-typedef struct {
-    unsigned char HashValue[HASH_SIZE];
-} Hash;
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
+//Modification eleve
+#define ADRESS_SIZE 50
 
-//structure de données pour une transaction
-typedef struct {
-    double amount; //montant de la transaction
-    char sender[ADRESS_SIZE]; // Nom de l'envoyeur 
-    char receiver[ADRESS_SIZE]; // Nom du destinataire 
-    long timestamp; //heure de création de la transaction
 
-    // unsigned char public_key[65]; //taille d'une clé public en bytes
-    // unsigned char signature[64]; //taille d'une signature en bytes
+// structure générique de liste non typée (à typer à l'utilisation)
+typedef struct Slist {
+	void * info; // les elt sont des blocks ou des transactions, ce qu'on veut en fait
+	struct Slist *next;
+} Slist;
+
+// structure de bloc
+typedef struct block {
+	int index; // numéro d'ordre
+	BYTE previousHash[SHA256_BLOCK_SIZE*2 + 1];
+  time_t timestamp;
+	int nbTx; // nombre de transaction dans le bloc
+	struct Slist * transactions; // liste de tx le nombre doit correspondre
+	BYTE merkleTree[SHA256_BLOCK_SIZE*2 + 1]; // hash de l'arbre de Merkle
+  BYTE blockHash[SHA256_BLOCK_SIZE*2 + 1]; // hash du bloc courant
+  char minerName[MAX_STRING]; // nom du mineur
+  char comment[MAX_STRING];
+	long nonce;
+} Block;
+
+typedef struct s_Blockchain {
+  int difficulty; // nombre de zéros initiaux du hash
+  int nbBlocks; // nombre de blocs de la Blockchain
+	struct Slist * blocklist; // liste des blocks
+  int reward4mining; // actual reward for the miner
+} Blockchain;
+
+typedef struct s_currency {
+	char currency_name[MAX_STRING];
+	long moneySupply; // masse monétaire en circulation
+	Blockchain * bc; // la blockchain
+} currency_t;
+
+typedef struct transaction {
+	BYTE txid[SHA256_BLOCK_SIZE*2 + 1]; // txid = hash(hash(tx))
+  time_t timestamp; // date de création
+	//time_t deadline; // date de péremption
+	BYTE adSender[SHA256_BLOCK_SIZE*2 + 1]; // ou nom "user x" pour la partie 1
+	BYTE adReceiver[SHA256_BLOCK_SIZE*2 + 1]; // idem
+  long txAmount; // en bit_thune
+  int nbInputs; // partie 2
+  struct Slist * lstInputs; // une ou plusieurs UTXO (parties 2 et 3)
+  int nbOutputs; // partie 2
+  struct Slist * lstOutputs; // limité à trois sorties : la tx+reward+change (parties 2 et 3)
+  char comment[MAX_STRING];
 } Transaction;
 
+typedef struct s_TxOutputs { //utxo (parties 2 et 3)
+    int outIndex; // numéro
+    char * lockingScript[LOCK_SCRIPT_SIZE];
+		time_t timestamp; // héritage de la tx
+    long amount; // in milliPass
+  } TxOutputs;
+
+typedef struct utxo{
+	BYTE hash[SHA256_BLOCK_SIZE*2 + 1]; // hash de la tx contenant l'UTXO
+	int indexOutput; // numéro d'ordre dans la list outputs
+	TxOutputs * txOut; // pointeur vers la txOut
+} Utxo;
+
+typedef struct account {
+  char str[MAX_STRING]; //mnémonic : plus facile à lire pour débuguer qu'une adresse
+  Slist * utxoList; // liste des tx non dépensées du compte
+  BYTE address[SHA256_BLOCK_SIZE*2 + 1]; // (parties 2 et 3)
+	BYTE priv_key[SHA256_BLOCK_SIZE*2 + 1]; // (parties 2 et 3)
+	BYTE pub_key[SHA256_BLOCK_SIZE*4 + 1]; // (parties 2 et 3)
+  long balance; // solde du compte
+} Account;
+
+//Modification eleve
 
 //Structure de données pour un utilisateur
 typedef struct {
@@ -36,30 +109,4 @@ typedef struct {
 } User;
 
 
-//structure de données pour un bloc
-typedef struct {
-    Hash previousHash; //hash du bloc précédent
-    Hash MerkleRoot; //racine de l'arbre de Merkle des transactions du bloc
-    char difficulty[4]; //difficulté du minage du bloc
-    int Nonce; //nombre utilisé pour trouver un hash valide (preuve de travail
-    long timestamp; //heure de création du bloc
-
-    //partie des transactions du bloc
-    int nbTransactions; //nombre de transactions dans le bloc
-    Transaction transactions[MAX_TRANSACTIONS]; //tableau de transactions du bloc "on en a dix maximum par bloc"
-
-    //partie hashage du bloc
-    Hash actualHash; //hash actuel du bloc
-
-    //liste chaînée des blocs
-    struct Bloc* next; //pointeur vers le bloc suivant
-}Bloc;
-
-//structure de données pour la blockchain
-typedef struct{
-    Bloc* head; //pointeur vers le premier bloc de la blockchain (genesis block)
-    int length; //nombre de blocs dans la blockchain
-} Blockchain;
-
-
-#endif
+#endif // __BC_DEFINES__
