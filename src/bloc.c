59589a608561f5e8a77fd2f0497189de6d80d01c
KasiaxX <kasialasicav@gmail.com>
2026-03-22 15:50:02 +0100

    mine_block

--- src/bloc.c
+++ src/bloc.c
@@ -5,7 +5,45 @@
 #include "define.h"
 #include "sha256_utils.h"
 
+void mine_block(Bloc *bloc) {
 
+    char buffer[1000]; // pour concatener les donnees du bloc
+    char hash_res[SHA256_BLOCK_SIZE * 2 + 1]; //stocker le resultat du hashage SHA256 en hexadécimal.
+    /* 
+    ' SHA256_BLOCK_SIZE ' = la taille donne dans le fichier sha256.h
+    ' *2 ' = on multiplie fois 2 car chaque octet binaire a c'est 2 caracteres hexadecimal 
+    ' +1 ' = on ajoute 1 pour '\0'
+    */
+
+    int success = 0; //pour touver le hash valide (difficulty = 4)
+
+    printf("Minage du bloc en cours...\n");
+
+    while (!success) { //"tant que on a pas trouver un bon hash"
+
+        //--- créer une chaîne avec les infos du bloc + le Nonce actuel ---
+        // sprintf pour mettre l'index, le timestamp, la difficulty et le Nonce dans le buffer
+        sprintf(buffer, "%s%ld%s%d", (char*)bloc->previousHash.HashValue, bloc->timestamp, bloc->difficulty, bloc->Nonce);
+
+        //--- calculer le hash de notre buffer
+        sha256ofString((BYTE *)buffer, hash_res);
+
+        //--- verifier si le hash commence par "0000" (difficulty = 4)
+        // comparerles 4 premiers caracteres de la chaîne hexadécimale
+        if (strncmp(hash_res, "0000", 4) == 0) {
+            success = 1; //hash trouvé commence par "0000"
+
+            // On enregistre le hash trouve dans le bloc
+            //HASH_SIZE -> a changer car HASH_SIZE = 32bytes et hash_res(hash cree) = 64bytes
+            strncpy((char*)bloc->actualHash.HashValue, hash_res, HASH_SIZE);
+
+            printf("Bloc miné ! -> Nonce: %d | Hash: %s\n", bloc->Nonce, hash_res);
+        } else {
+
+            bloc->Nonce++; // On incremente et on recommence
+        }
+    }
+}
 
 Bloc * create_genesis_block(){
 
