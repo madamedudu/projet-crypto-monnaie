9c8c1b9b04df4185db3ca20f16b0662d95544777
ZyadMA <zyad.mohamed-abdelmoneim@univ-tlse3.fr>
2026-05-18 11:18:13 +0200

    feat(T4): implémentation de la pile générique pour l'interprétation des scripts

---
+++ include/script.h
@@ -0,0 +1,22 @@
+#ifndef SCRIPT_H
+#define SCRIPT_H
+
+#include "define.h"
+
+#define MAX_STACK 16
+#define MAX_ITEM_LEN 132 // 128 pour les données + 4 pour les préfixes d'opcode
+
+// Structure de la pile
+typedef struct {
+    char data[MAX_STACK][MAX_ITEM_LEN];
+    int top; // Index du sommet de la pile (-1 si vide)
+} Stack;
+
+//fonctions de gestion de la pile
+void stack_init(Stack *s);
+int stack_push(Stack *s, const char *value);
+int stack_pop(Stack *s, char *out);
+int stack_peek(Stack *s, char *out);
+int stack_is_empty(Stack *s);
+
+#endif
