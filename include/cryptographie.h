44afe743833ddda883b89eea2673a8366b204664
Maeva_Rastouil <maevarastouil@gmail.com>
2026-05-28 20:40:54 +0200

    signature + verification

--- include/cryptographie.h
+++ include/cryptographie.h
@@ -8,9 +8,10 @@ typedef unsigned char byte;
 
 void calc_address(char address[35], char *pub_key_char);
 void generer_cles(Account *compte);
-void signer_transaction(Transaction *transaction, BYTE *cle_privee, BYTE *signature);
 void creer_lock_script(TxOutputs *out, char *signature, char *pubkey);
 void creer_unlock_script(TxInputs *in,char * pubkey_hash);
 void script_to_string(char **script, int size, char *out, int out_size);
+int signer_transaction_ecdsa(Transaction *transaction, BYTE *cle_privee_hex, char *signature_hex);
+int verifier_transaction_ecdsa(Transaction *transaction, BYTE *cle_publique_hex, char *signature_hex);
 
 #endif
